bd9d4069112e115665c2d839d5f9d6c17fa9c611
kent1db <kent1db@gmail.com>
2022-01-31 15:59:24 +0100

    restruct code in structure, and start setting status and content type

--- includes/send_page.hpp
+++ includes/send_page.hpp
@@ -6,8 +6,8 @@
 #define WEBSERV_SEND_PAGE_HPP
 #include "Response.hpp"
 
-int display_page(int new_socket, std::map<std::string, std::string> request, bool autoindex, Response &response);
-std::string ft_openFile(std::string path, std::string status, std::string content_type, Response &response);
+void display_page(int new_socket, std::map<std::string, std::string> request, bool autoindex, Response &response);
+void ft_openFile(std::string path, std::string content_type, Response &response);
 std::string ft_header(int length, std::string s, std::string content_type);
 
 #endif //WEBSERV_SEND_PAGE_HPP
--- srcs/send_page.cpp
+++ srcs/send_page.cpp
@@ -4,34 +4,35 @@
 
 #include "webserv.hpp"
 
-int 	checkError(std::string &path){
-	if (path.find("//") != std::string::npos)
+int 	checkError(std::string &path, Response &response){
+	if (path.find("//") != std::string::npos) {
+		response.setStatus("404 Not Found");
 		return (1);
+	}
 	return (0);
 }
 
-std::string ft_openFile(std::string path, std::string status, std::string content_type, Response &response){
+void ft_openFile(std::string path, std::string content_type, Response &response){
     std::ifstream ifs(path);
     std::string page;
     if (ifs) {
         std::ostringstream oss;
         oss << ifs.rdbuf();
         std::string file = oss.str();
-        return (response.fillHeader(file, status, content_type) + file);
+        response.fillHeader(file, response.getStatus(), content_type);
+		response.setBody(file);
     }
-    return (page);
 }
 
-int display_page(int new_socket, std::map<std::string, std::string> request, bool autoindex, Response &response){
-    std::string file;
+void display_page(int new_socket, std::map<std::string, std::string> request, bool autoindex, Response &response){
     string path = request["path"];
-    std::string status = "200 OK ";
+//    std::string status = "200 OK ";
 	DIR *dir;
-	if (checkError(path))
-		file = ft_openFile("./pages/404.html", "404 Not Found", "text/html", response);
+	if (checkError(path, response))
+		ft_openFile("./pages/404.html", "text/html", response);
 	else if (autoindex && (dir = opendir(const_cast<char *>(("." + path).c_str()))) != NULL)
-		file = create_indexing_page(dir, path, response);
+		create_indexing_page(dir, path, response);
 	else
-		file = create_existing_page(path, status, response);
-	return (write(new_socket, const_cast<char *>(file.c_str()), file.length()));
+		create_existing_page(path, response);
+	write(new_socket, const_cast<char *>(response.getResponse().c_str()), response.getLength());
 }
