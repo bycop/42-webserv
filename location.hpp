40b0e8bce5b78738575c60679e60fc0425014154
Bycop <thebycop@gmail.com>
2022-01-31 13:46:14 +0100

    Exit on error + parsing path location

--- location.hpp
+++ location.hpp
@@ -15,12 +15,14 @@ public:
 		std::cout << "index: " << this->index << std::endl;
 		std::cout << "root: " << this->root << std::endl;
 		std::cout << "default_pages: " << this->allow_methods << std::endl;
+		std::cout << "path: " << this->path << std::endl;
 	};
 
 	bool autoindex;
 	std::string index;
 	std::string root;
 	std::string allow_methods;
+	std::string path;
 };
 
 #endif
--- parser.cpp
+++ parser.cpp
@@ -1,159 +1,181 @@
 #include <fstream>
 
 #include "data.hpp"
+#include <cctype>
+
 #define _countof(a) (sizeof(a) / sizeof(*(a)))
 
+bool error = false;
+
+void display_error(std::string type, std::string key) {
+	std::cout << "\e[91m[ERROR]\e[0m Unrecognized " << type << " key at '"
+			  << key << "'" << std::endl;
+	error = true;
+}
+
 void setters(std::string newdata, int type, Data *data) {
-  switch (type) {
-    case 0:
-      data->user = newdata;
-      break;
-    case 1:
-      data->pid = newdata;
-      break;
-    case 2:
-      data->error_log = newdata;
-      break;
-  }
+	switch (type) {
+		case 0:
+			data->user = newdata;
+			break;
+		case 1:
+			data->pid = newdata;
+			break;
+		case 2:
+			data->error_log = newdata;
+			break;
+	}
 }
 
 void server_setters(std::string newdata, int type, Server *server) {
-  switch (type) {
-    case 0:
-      server->host = newdata;
-      break;
-    case 1:
-      server->port = newdata;
-      break;
-    case 2:
-      server->server_name = newdata;
-      break;
-    case 3:
-      server->default_pages = newdata;
-      break;
-    case 4:
-      server->size_limit = newdata;
-      break;
-  }
+	switch (type) {
+		case 0:
+			server->host = newdata;
+			break;
+		case 1:
+			server->port = newdata;
+			break;
+		case 2:
+			server->server_name = newdata;
+			break;
+		case 3:
+			server->default_pages = newdata;
+			break;
+		case 4:
+			server->size_limit = newdata;
+			break;
+	}
 }
 
 void location_setters(std::string newdata, int type, Location *location) {
-  switch (type) {
-    case 0:
-      if (newdata == "on")
-        location->autoindex = true;
-      else
-        location->autoindex = false;
-      break;
-    case 1:
-      location->index = newdata;
-      break;
-    case 2:
-      location->root = newdata;
-      break;
-    case 3:
-      location->allow_methods = newdata;
-      break;
-  }
+	switch (type) {
+		case 0:
+			if (newdata == "on")
+				location->autoindex = true;
+			else
+				location->autoindex = false;
+			break;
+		case 1:
+			location->index = newdata;
+			break;
+		case 2:
+			location->root = newdata;
+			break;
+		case 3:
+			location->allow_methods = newdata;
+			break;
+	}
 }
 
-void location_loop(std::ifstream &file, Server *server) {
-  Location location;
-  std::string line;
-  std::string locationvars[4] = {"autoindex", "index", "root", "allow_methods"};
-  bool find;
-  std::cout << _countof(locationvars) << std::endl;
+std::string location_path(std::string line) {
 
-  while (std::getline(file, line)) {
-    std::string original = line;
+	std::string path;
+	size_t space = 0;
+	for (int i = 0; i < line.length() && isspace(line[i]); i++)
+		space++;
+	if (line.find("location", space) == std::string::npos || line.find("{", space) == std::string::npos)
+		return (path);
+	path = &line[9 + space];
+	path = path.erase(path.find("{"), path.length());
+	path.erase(std::remove_if(path.begin(), path.end(), ::isspace), path.end());
+	return (path);
+}
 
-    find = false;
-    line.erase(std::remove_if(line.begin(), line.end(), ::isspace), line.end());
-    for (int i = 0; i < _countof(locationvars); i++)
-      if (line.find(locationvars[i] + ":", 0) == 0) {
-        line = &line[locationvars[i].length() + 1];
-        location_setters(line, i, &location);
-        find = true;
-      }
-    if (line.find("}", 0) == 0)
-      break;
-    else if (!find && line != "")
-      std::cout << "\e[93m[WARNING]\e[0m Unrecognized location key at '"
-                << original << "'" << std::endl;
-  }
+void location_loop(std::ifstream &file, Server *server, std::string path) {
+	Location location;
+	std::string line;
+	std::string locationvars[4] = {"autoindex", "index", "root", "allow_methods"};
+	bool find;
 
-  server->locations.push_back(location);
+	location.path = path;
+	while (std::getline(file, line)) {
+		std::string original = line;
+
+		find = false;
+		line.erase(std::remove_if(line.begin(), line.end(), ::isspace), line.end());
+		for (int i = 0; i < _countof(locationvars); i++)
+			if (line.find(locationvars[i] + ":", 0) == 0) {
+				line = &line[locationvars[i].length() + 1];
+				location_setters(line, i, &location);
+				find = true;
+			}
+		if (line.find("}", 0) == 0)
+			break;
+		else if (!find && line != "")
+			display_error("location", original);
+	}
+	server->locations.push_back(location);
 }
 
 void server_loop(std::ifstream &file, Data *data) {
-  Server server;
-  std::string line;
-  std::string servervars[6] = {"host", "port", "server_name", "default_pages",
-                               "size_limit", "redirect"};
-  bool find;
+	Server server;
+	std::string line;
+	std::string servervars[6] = {"host", "port", "server_name", "default_pages",
+								 "size_limit", "redirect"};
+	bool find;
+	while (std::getline(file, line)) {
+		std::string original = line;
 
-  while (std::getline(file, line)) {
-    std::string original = line;
+		find = false;
+		line.erase(std::remove_if(line.begin(), line.end(), ::isspace), line.end());
+		for (int i = 0; i < _countof(servervars); i++)
+			if (line.find(servervars[i] + ":", 0) == 0) {
+				line = &line[servervars[i].length() + 1];
+				server_setters(line, i, &server);
+				find = true;
+			}
+		if (line.find("}", 0) == 0) break;
+		if (!find && line.find("location", 0) == 0 && !location_path(original).empty())
+			location_loop(file, &server, location_path(original));
+		else if (!find && line != "")
+			display_error("server", original);
 
-    find = false;
-    line.erase(std::remove_if(line.begin(), line.end(), ::isspace), line.end());
-    for (int i = 0; i < _countof(servervars); i++)
-      if (line.find(servervars[i] + ":", 0) == 0) {
-        line = &line[servervars[i].length() + 1];
-        server_setters(line, i, &server);
-        find = true;
-      }
-    if (line.find("}", 0) == 0) break;
-    if (!find && line.find("location{", 0) == 0)
-      location_loop(file, &server);
-    else if (!find && line != "")
-      std::cout << "\e[93m[WARNING]\e[0m Unrecognized server key at '"
-                << original << "'" << std::endl;
-  }
+	}
 
-  data->servers.push_back(server);
+	data->servers.push_back(server);
 }
 
 int main(int ac, char **av) {
-  std::string file_path;
-  if (ac == 2) {
-    file_path = av[1];
-  } else {
-    std::cout << "Usage: ./parser <file_path>" << std::endl;
-    return (1);
-  }
+	std::string file_path;
+	if (ac == 2) {
+		file_path = av[1];
+	} else {
+		std::cout << "Usage: ./parser <file_path>" << std::endl;
+		return (1);
+	}
 
-  std::ifstream file(file_path);
+	std::ifstream file(file_path);
 
-  if (!file.is_open()) {
-    std::cout << "Error: file not found" << std::endl;
-    return (1);
-  }
+	if (!file.is_open()) {
+		std::cout << "Error: file not found" << std::endl;
+		return (1);
+	}
 
-  Data data;
-  std::string line;
-  bool find;
+	Data data;
+	std::string line;
+	bool find;
 
-  std::string vars[3] = {"user", "pid", "error_log"};
+	std::string vars[3] = {"user", "pid", "error_log"};
 
-  while (std::getline(file, line)) {
-    std::string original = line;
-    find = false;
-    line.erase(std::remove_if(line.begin(), line.end(), ::isspace), line.end());
-    for (int i = 0; i < _countof(vars) && !find; i++)
-      if (line.find(vars[i] + ":", 0) == 0) {
-        line = &line[vars[i].length() + 1];
-        setters(line, i, &data);
-        find = true;
-      }
-    if (!find && line.find("server{", 0) == 0)
-      server_loop(file, &data);
-    else if (!find && line != "")
-      std::cout << "\e[93m[WARNING]\e[0m Unrecognized main key at '" << original
-                << "'" << std::endl;
-  }
+	while (std::getline(file, line)) {
+		std::string original = line;
+		find = false;
+		line.erase(std::remove_if(line.begin(), line.end(), ::isspace), line.end());
+		for (int i = 0; i < _countof(vars) && !find; i++)
+			if (line.find(vars[i] + ":", 0) == 0) {
+				line = &line[vars[i].length() + 1];
+				setters(line, i, &data);
+				find = true;
+			}
+		if (!find && line.find("server{", 0) == 0)
+			server_loop(file, &data);
+		else if (!find && line != "")
+			display_error("main", original);
+	}
+	if (error)
+		std::cout << "Error: Exiting program" << std::endl;
+	else
+		data.print();
 
-  data.print();
-
-  return (0);
+	return (0);
 };
