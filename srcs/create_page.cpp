df096ee6ae2f62cd1dc58bdcdd1efa82d3307188
Bycop <thebycop@gmail.com>
2022-02-24 11:28:25 +0100

    Dev Web BB

--- srcs/create_page.cpp
+++ srcs/create_page.cpp
@@ -10,8 +10,10 @@ void create_indexing_page(DIR *dir, std::string &path, Response &response){
 
 	if (pathTmp[0] == '.')
 		pathTmp.erase(0, 1);
-	mypage << "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<title>Index of " << pathTmp << "</title>\n</head>\n" << std::endl;
-	mypage << "<body><h1>Index of " << pathTmp << "\n</h1><hr/><br>";
+	mypage << "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<title>Index of " << pathTmp << "</title>\n"
+																							"<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css\" integrity=\"sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==\" crossorigin=\"anonymous\" referrerpolicy=\"no-referrer\" />\n"
+																							"</head>\n" << std::endl;
+	mypage << "<body><h1> <i class=\"fa-solid fa-triangle-exclamation\"></i> Index of " << pathTmp << "\n</h1><hr/><br>";
 	struct dirent *ent;
 	//Ne pas afficher le "."
 	ent = readdir(dir);
@@ -19,9 +21,11 @@ void create_indexing_page(DIR *dir, std::string &path, Response &response){
 	while ((ent = readdir(dir)) != NULL) {
 		std::string name = ent->d_name;
 		if (opendir(const_cast<char *>((path + name).c_str())) != NULL && name != "..")
-			mypage << "<a href=\"" << pathTmp << name << "\">" << ent->d_name << "/" << "</a><br>";
+			mypage << "<a href=\"" << pathTmp << name << "\">" << "<i class=\"fa-solid fa-folder\"></i>  " <<ent->d_name << "/" << "</a><br>";
+		else if (name != "..")
+			mypage << "<a href=\"" << pathTmp << name << "\">" << "<i style='font-size: 19px' class=\"fa-solid fa-file-lines\"></i>  " << ent->d_name << "</a><br>";
 		else
-			mypage << "<a href=\"" << pathTmp << name << "\">" << ent->d_name << "</a><br>";
+			mypage << "<a href=\"" << pathTmp << name << "\">" << "<i style='font-size: 24px' class=\"fa-solid fa-circle-chevron-left\"></i>" << "</a><br><br>";
 	}
 	closedir (dir);
 	mypage << "<br><hr/></body></html>\n";
