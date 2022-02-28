9ad6036e6a08cb532a954008f3476c54886ae510
Bycop <thebycop@gmail.com>
2022-02-28 11:33:18 +0100

    Change static value to config value tester

--- OurTester/main.py
+++ OurTester/main.py
@@ -1,4 +1,5 @@
 import requests
+import config
 from time import sleep
 from tqdm import tqdm
 import socket
@@ -9,22 +10,22 @@ from http.client import HTTPResponse
 #        "9\r\n" + "Developer\r\n" + \
 #        "7\r\n" + "Network\r\n" + \
 #        "0\r\n" + "\r\n"
-def make_request(method, url, printbool, expected, data=None):
+def make_request(method, url, expected, data=None):
     r = 0
     try:
-        if (printbool):
+        if config.PRINT_REQUESTS:
             print("Testing " + method + " on " + url)
         if method == 'GET':
-            r = requests.get(url, timeout=1)
+            r = requests.get(url, timeout=config.REQUEST_TIMEOUT)
         elif method == 'POST':
-            r = requests.post(url, data=data, timeout=1)
+            r = requests.post(url, data=data, timeout=config.REQUEST_TIMEOUT)
         elif method == 'DELETE':
-            r = requests.delete(url, timeout=1)
+            r = requests.delete(url, timeout=config.REQUEST_TIMEOUT)
         elif method == 'PUT':
-            r = requests.put(url, data=data, timeout=1)
+            r = requests.put(url, data=data, timeout=config.REQUEST_TIMEOUT)
         elif method == 'HEAD':
-            r = requests.head(url, timeout=1)
-        if printbool:
+            r = requests.head(url, timeout=config.REQUEST_TIMEOUT)
+        if config.PRINT_REQUESTS:
             print(r.status_code)
         if r.status_code != expected:
             return False
@@ -33,14 +34,17 @@ def make_request(method, url, printbool, expected, data=None):
         return False
     return True
 
-def send_request(request_header, printbool, expected):
+def send_request(request_header, expected):
+    if config.PRINT_REQUESTS:
+        print("Testing " + request_header + " on " + config.SERVER_ADDRESS + ":" + config.SERVER_PORT)
+
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("localhost", 8080))
+    client.connect((config.SERVER_ADDRESS, config.SERVER_PORT))
     client.send(request_header.encode())
     # read and parse http response
     http_response = HTTPResponse(client)
     http_response.begin()
-    if (printbool):
+    if config.PRINT_REQUESTS:
         print(http_response.status)
     if http_response.status != expected:
         return False
@@ -51,26 +55,26 @@ def send_request(request_header, printbool, expected):
 
 basic_tests = [
     # Miscelaneous tests
-    ['PUT', 'http://localhost:8080/pages', 0, 501],  # 1
-    ['HEAD', 'http://localhost:8080/pages/1', 0, 501],  # 2
+    ['PUT', 'http://localhost:8080/pages', 501],  # 1
+    ['HEAD', 'http://localhost:8080/pages/1', 501],  # 2
 
     # GET tests
-    ['GET', 'http://localhost:8080/', 0, 200],  # 3
-    ['GET', 'http://localhost:8080/missing_rights/', 0, 403],  # 4
+    ['GET', 'http://localhost:8080/', 200],  # 3
+    ['GET', 'http://localhost:8080/missing_rights/', 403],  # 4
 
     # DELETE tests
-    ['DELETE', 'http://localhost:8080/post/post.html', 0, 405],  # 5
-    ['DELETE', 'http://localhost:8080/delete/delete2.html', 0, 404],  # 6
-    ['DELETE', 'http://localhost:8080/post-delete/post-delete2.html', 0, 404],  # 7
+    ['DELETE', 'http://localhost:8080/post/post.html', 405],  # 5
+    ['DELETE', 'http://localhost:8080/delete/delete2.html', 404],  # 6
+    ['DELETE', 'http://localhost:8080/post-delete/post-delete2.html', 404],  # 7
 
     # POST tests
-    ['POST', 'http://localhost:8080/post-delete/post-delete.html', 0, 200],  # 8
-    ['POST', 'http://localhost:8080/post/post.html', 0, 200],  # 9
-    ['POST', 'http://localhost:8080/delete/delete.html', 0, 405],  # 10
+    ['POST', 'http://localhost:8080/post-delete/post-delete.html', 200],  # 8
+    ['POST', 'http://localhost:8080/post/post.html', 200],  # 9
+    ['POST', 'http://localhost:8080/delete/delete.html', 405],  # 10
 ]
 
 complete_tests = [
-    ["GET / HTTP/1.1\r\nHost: localhost:8080\r\n\r\n", 0, 200]
+    ["GET / HTTP/1.1\r\nHost: localhost:8080\r\n\r\n", 200]
 ]
 
 output = ""
