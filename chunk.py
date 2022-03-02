e5a63a810738ff6e96b9a0ea2516a5fbfc7442c5
Bycop <thebycop@gmail.com>
2022-03-02 14:43:16 +0100

    delete chunk

--- chunk.py
+++
@@ -1,8 +0,0 @@
-import requests
-data = "7\r\n" + "Mozilla\r\n" + \
-       "B\r\n" + "Developer\r\n\r\n" + \
-       "7\r\n" + "Network\r\n" + \
-       "0\r\n" + "\r\n"
-headers = {'Transfer-Encoding': 'chunked'}
-r = requests.post('http://localhost:8080', data=data, headers=headers)
-print(r.text)
